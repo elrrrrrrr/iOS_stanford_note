26be2d30014b7b8abc8ae195ea79a85bc690d9da
elr <elrrrrrrr@gmail.com>
2014-04-09 13:30:45 +0800

    complete ch5 note

--- Attributor/Attributor/ViewController.m
+++ Attributor/Attributor/ViewController.m
@@ -12,8 +12,10 @@
 @property (weak, nonatomic) IBOutlet UITextView *body;
 @property (weak, nonatomic) IBOutlet UILabel *headLine;
 - (IBAction)changeBodyColor:(UIButton *)sender;
+- (IBAction)unoutlineButtonAction:(id)sender;
 
 - (IBAction)outlineButtonAction:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 
 
 @end
@@ -24,17 +26,48 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString *title =  [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title setAttributes:@{NSStrokeColorAttributeName:self.outlineButton.tintColor,NSStrokeWidthAttributeName:@3} range:NSMakeRange(0, title.length)];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+}
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+}
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(preferredFontsChanged:) name:UIContentSizeCategoryDidChangeNotification object:nil];
+
 }
 
+- (void)preferredFontsChanged:(NSNotificationCenter *)notifications
+{
+    [self usePreferredFonts];
+}
+-(void)usePreferredFonts
+{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headLine.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIContentSizeCategoryDidChangeNotification object:nil];
+}
 - (IBAction)changeBodyColor:(UIButton *)sender {
     [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:[self.body selectedRange]];
+}
+
+- (IBAction)unoutlineButtonAction:(id)sender {
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName range:[self.body selectedRange]];
 }
 
 - (IBAction)outlineButtonAction:(id)sender {
--- CH5/README.md
+++ CH5/README.md
@@ -8,3 +8,42 @@ UITextView
 
 NSNotification
 -------------
+
+ViewController 生命周期
+------------------
+建立
+设置outlet
+appearing and disappering
+Geometry changes
+low-memory situations
+
+
+after instantiation and outlet
+
+viewDidload is called 
+always [super viewDidLoad]
+just called once 
+不要在didload添加定位
+
+viewWillAppear:
+不添加一次性内容 （放在didload里）
+主要进行同步
+viewWillDisappear
+
+对应didAppear
+
+viewWill(did)LayoutSubview
+屏幕旋转
+
+didReceiveMemoryWarning
+内存警告
+
+awakeFromNib before MVC is "load"
+
+
+
+NSNotification (radio)
+--------------
+NSNotificationCenter
+-(void)addObserver:(id)observer
+[center removeObserver:self]
