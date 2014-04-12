171ae8bd14f171f02949da43f614b3fa556e9409
elr <elrrrrrrr@gmail.com>
2014-04-12 23:01:01 +0800

    add supercard corner

--- SuperCard/SuperCard/PlayingCardView.h
+++ SuperCard/SuperCard/PlayingCardView.h
@@ -9,7 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface PlayingCardView : UIView
-
 @property (nonatomic) NSUInteger rank;
 @property (strong,nonatomic) NSString *suit;
 @property (nonatomic) BOOL faceUp;
--- SuperCard/SuperCard/PlayingCardView.m
+++ SuperCard/SuperCard/PlayingCardView.m
@@ -11,6 +11,21 @@
 @implementation PlayingCardView
 
 
+-(void)setSuit:(NSString *)suit
+{
+    _suit = suit;
+    [self setNeedsDisplay];
+}
+-(void)setRank:(NSUInteger)rank
+{
+    _rank = rank;
+    [self setNeedsDisplay];
+}
+-(void)setFaceUp:(BOOL)faceUp
+{
+    _faceUp = faceUp;
+    [self setNeedsDisplay];
+}
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
@@ -19,8 +34,43 @@
     }
     return self;
 }
+#define CORNER_FONT_STANDARD_HEIGHT 180.0
+#define CORNER_RADIUS 12.0
+-(CGFloat)cornerScaleFactor {return self.bounds.size.height / CORNER_FONT_STANDARD_HEIGHT;}
+-(CGFloat)cornerRadius { return CORNER_RADIUS * [self cornerScaleFactor] ;}
+-(CGFloat)cornerOffset { return [self cornerRadius] / 3.0 ;}
+-(void)drawRect:(CGRect)rect
+{
+    UIBezierPath *roundedRect = [UIBezierPath bezierPathWithRoundedRect:self.bounds cornerRadius:[self cornerRadius]];
+    [roundedRect addClip];
+    [[UIColor whiteColor] setFill];
+    UIRectFill(self.bounds);
+    
+    [[UIColor blackColor] setStroke];
+    [roundedRect stroke];
+    [self drawCorners];
+}
 
+-(void)drawCorners
+{
+    NSMutableParagraphStyle *par = [[NSMutableParagraphStyle alloc] init];
+    par.alignment = NSTextAlignmentCenter;
+    UIFont *cor = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
 
+}
+
+#pragma mark - init
+-(void)setup
+{
+    self.backgroundColor = nil;
+    self.opaque = NO;
+    self.contentMode = UIViewContentModeRedraw;
+}
+
+-(void)awakeFromNib
+{
+    [self setup];
+}
 
 /*
 // Only override drawRect: if you perform custom drawing.
